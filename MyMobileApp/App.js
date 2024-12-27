22ba02f0f307502fe79903257d1383257bf32daa
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2024-12-27 16:46:33 +0530

    Implement ScrollView to enable vertical scrolling for content

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
     <PaperProvider>
+      <ScrollView>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
     </PaperProvider>
   );
 }
